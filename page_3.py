8eee09388e028cf38c7a1310ba8e64b233f0f5cb
ViphuKN <viphu12@gmail.com>
2021-04-30 14:54:00 -0400

    Trying to get the api to work

--- page_3.py
+++ page_3.py
@@ -1,4 +1,39 @@
 from tkinter import *
+import requests
+import json
+
+dogs_apik = '100ae381-76de-48ed-8a6d-73c6e11af170'
+response = requests.get("https://api.thedogapi.com/v1/breeds")
+
+class Application(Tk):
+    """ To run the application of the GUI
+    """
+    def __init__(self):
+        Tk.__init__(self)
+        self.geometry('500x500')
+        self.title('Welcome to the Find Dog Breed App')
+        
+        first_label = tk.Label(self, text = "Results of the Dog Breeds", font=10) #Intro login screen
+        first_label.pack(padx = 3, pady = 3) #Pads for the Intro login screen
+        count = 1
+        for x in response.json():
+            #print(count, "Dog's name: " + x["name"]) #Dog's name
+            while count < 6: #Only display 5 dogs (for testing)
+                print([count], x["name"] + "'s temperament: " + x["temperament"]) #Dog's temperament
+                count += 1    
+    
+        first_button = tk.Button(self, text ="Button", command = nextpage) #A button to click after login info is completed
+        first_button.pack(padx= 5, pady = 5)
+
+
+Label(
+    ws,
+    text="This is fourth page",
+    padx=20,
+    pady=20,
+    bg='#ffbf00',
+    font=f
+).pack(expand=True, fill=BOTH)
 
 ws = Tk()
 ws.geometry('400x300')
@@ -8,21 +43,16 @@ ws['bg']='#ffbf00'
 f = ("Times bold", 14)
  
 def nextPage():
+    """ Go to the next page
+    """
     ws.destroy()
-    import page_1
+    import page_4
 
 def prevPage():
+    """ Go to the previous page
+    """
     ws.destroy()
     import page_2
-
-Label(
-    ws,
-    text="This is third page",
-    padx=20,
-    pady=20,
-    bg='#ffbf00',
-    font=f
-).pack(expand=True, fill=BOTH)
 
 Button(
     ws, 
@@ -37,4 +67,8 @@ Button(
     command=nextPage
     ).pack(fill=X, expand=TRUE, side=LEFT)
 
-ws.mainloop()
+
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
+    ws.mainloop()
