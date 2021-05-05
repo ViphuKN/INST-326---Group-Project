05963536d7ec693af66e2bac35f13476e0102a87
Gerson Roldan <groldan@terpmail.umd.edu>
2021-05-05 01:16:33 -0400

    updates to TheDogsApi file for data wrangling + new file for storing lines of code

--- TheDogsApi.py
+++ TheDogsApi.py
@@ -3,53 +3,80 @@ import json
 dogs_apik = '100ae381-76de-48ed-8a6d-73c6e11af170'
 # print(response.json()["name"])
 response = requests.get("https://api.thedogapi.com/v1/breeds")
-# response1 = requests.get("https://api.thedogapi.com/v1/breeds/1")
-# print(response.text)
-# print(response.status_code)
-# print(response.reason)
-# print(response.headers)
-# print(response.headers.get("Content-Type"))
+dogs_data = response.json()
+#lists for filtering
+dogs_list = []
+unlisted_dogs = []
+behaviors = set()
+breeds = set()
 
-# print(response.json())
-# list_of_dogs = response.json()
-# for dog in list_of_dogs:
-#     print(dog)
 
-# print(response1.json()["temperament"])
-# print(response1.json())
+### data filtering changes
+def filter_breeds(data):
+    """Filters Through dogs_data to take care of missing breed data
+    empty strings (only one case) are labeled msc, and missing breed_group keys are
+    reconcilled with msc.
 
-dog_list = []
-dog_behavior = []
-set_db = set(dog_behavior)
-
-#This is to display the dog names and also their temperament
-count = 1
-for x in response.json():
-    #print(count, "Dog's name: " + x["name"]) #Dog's name
-    while count < 6: #Only display 5 dogs (for testing)
-        print([count], x["name"] + "'s temperament: " + x["temperament"]) #Dog's temperament
-        count += 1    
-    
-        """
+    There are technically 7 breeds total with 8 & 9 being mixed and msc. (in a set so unordered really)
+    """
+    for dog in data:
         try:
-            print(x["name"] + "'s temperament: " + x["temperament"]) #Dog's temperament
+            dog["breed_group"]
+            if dog["breed_group"] == "":
+                dog["breed_group"] = "Miscellaneous"
+            breeds.add(dog["breed_group"])
         except KeyError:
-            print("DID NOT HAVE TEMPERAMENT") #If the dog doesn't have a temperament
-        count+=1
-        # print(x["life_span"])
-        # dog_list.append(x)
-        """
+            # print(dog["name"] + " DID NOT HAVE A BREED!")
+            dog["breed_group"] = "Miscellaneous"
+            breeds.add(dog["breed_group"])
 
-#length = len(response.json())
-#num = 0
-# while num <= length:
-#     if response.json()[num]["temperament"]:
-#         print(response.json()[num]["temperament"])
-#     else:
-#         print("none")
-#     num+=1
+filter_breeds(dogs_data)
+### test to see if filter_breeds() works ###
+# print("breeds")
+# print(len(breeds))
+# for x in breeds:
+#     print(x)
+# print(breeds)
 
-# print(length)
-# print(dog_list[0]["name"])
-# print(dog_list[0]["temperament"])
+
+#For figuring out what dogs have temperments missing
+def filter_temperament(data):
+    for dog in data:
+        try:
+            dog["temperament"]
+            if dog["temperament"] == "":
+                dog["temperament"] = "unknown"
+            behaviors.add(dog["temperament"])
+        except KeyError:
+            # print(dog["name"] + " DID NOT HAVE A TEMPERAMENT__!")
+            dog["temperament"] = "unknown"
+            behaviors.add(dog["temperament"])
+
+filter_temperament(dogs_data)
+### test to see if filter_temperment() works ###
+# print(len(behaviors))
+# for x in dogs_data:
+#     if x["temperament"] == "unknown":
+#         print("true")
+### data filtering changes
+
+###old
+#This is to display the dog names and also their temperament
+# count = 1
+# for x in response.json():
+#     #print(count, "Dog's name: " + x["name"]) #Dog's name
+#     while count < 6: #Only display 5 dogs (for testing)
+#         print([count], x["name"] + "'s temperament: " + x["temperament"]) #Dog's temperament
+#         count += 1    
+    
+#         """
+#         try:
+#             print(x["name"] + "'s temperament: " + x["temperament"]) #Dog's temperament
+#         except KeyError:
+#             print("DID NOT HAVE TEMPERAMENT") #If the dog doesn't have a temperament
+#         count+=1
+#         # print(x["life_span"])
+#         # dog_list.append(x)
+#         """
+
 
---
+++ archived_scripts.py
@@ -0,0 +1,14 @@
+# response1 = requests.get("https://api.thedogapi.com/v1/breeds/1")
+# print(response.text)
+# print(response.status_code)
+# print(response.reason)
+# print(response.headers)
+# print(response.headers.get("Content-Type"))
+
+# print(response.json())
+# list_of_dogs = response.json()
+# for dog in list_of_dogs:
+#     print(dog)
+
+# print(response1.json()["temperament"])
+# print(response1.json())
