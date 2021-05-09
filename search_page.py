32d1e7b4a98bb42322cd6535f54698563ffc58bb
ViphuKN <viphu12@gmail.com>
2021-05-09 16:59:48 -0400

    Created a search page for the user to choose what dog they prefer

---
+++ search_page.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import requests
+import json
+
+"""Search for the dog breed"""
+
+class Application(tk.Tk):
+    """ To run the application of the GUI
+    """
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.geometry('500x500')
+        self.title('Search Page')
+        
+        #Intro Menu Screen
+        first_label = tk.Label(self, text = "INSERT TEXT HERE", font=10)  #Try to have search for size, temperament, and breed
+        #Pads for the Intro login screen
+        first_label.pack(padx = 3, pady = 3) 
+
+        #A button to go to the search page
+        first_button = tk.Button(self, text ="Show Results", command = results) 
+        first_button.pack(padx= 5, pady = 5)
+        
+        #A button to go to the logout page
+        second_button = tk.Button(self, text ="Favorites", command = favorite) 
+        second_button.pack(padx= 5, pady = 5)
+        
+        #A button to go to the logout page
+        third_button = tk.Button(self, text ="Logout", command = logout) 
+        third_button.pack(padx= 5, pady = 5)
+    
+def results():
+    """ Go to the main menu page
+    """
+    #This is to close the current page
+    #tk.destroy() #Not working b/c it does not have destroy attribute 
+    import results_page
+
+def favorite():
+    """ Go to the favorite page
+    """
+    import favorite_page
+    
+def logout():
+    """ Direct the user to the welcome page if the user want to logout
+    """
+    import welcome_page
+      
+if __name__ == "__main__":
+    app = Application()
+    app.mainloop()
