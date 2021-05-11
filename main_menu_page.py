3605c67ed403f0fb4a345ac2a9b778439d4fa0d9
ViphuKN <viphu12@gmail.com>
2021-05-11 15:21:15 -0400

    Removed a page

--- main_menu_page.py
+++ main_menu_page.py
@@ -20,18 +20,11 @@ class Application(tk.Tk):
         #A button to go to the search page
         first_button = tk.Button(self, text ="Search", command = self.search) 
         first_button.pack(padx= 5, pady = 5)
-        #A button to go to the favorite page
-        second_button = tk.Button(self, text ="Favorite", command = self.favorite) 
-        second_button.pack(padx= 5, pady = 5)
+
         #A button to go to the logout page
         third_button = tk.Button(self, text ="Logout", command = self.logout) 
         third_button.pack(padx= 5, pady = 5)
         
-    def favorite(self):
-        """ Go to the favorite page
-        """
-        tk.Tk.destroy(self) #This is to close the current page        
-        import favorite_page
         
     def search(self):
         """ Go to the search page to select a dog breed
