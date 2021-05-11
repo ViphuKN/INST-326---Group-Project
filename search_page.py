bb42da2d5a8b7b540f7d1da16af128b9decef555
ViphuKN <viphu12@gmail.com>
2021-05-11 15:10:31 -0400

    Attempted to do drop down features

--- search_page.py
+++ search_page.py
@@ -17,6 +17,24 @@ class Application(tk.Tk):
         #Pads for the Intro login screen
         first_label.pack(padx = 3, pady = 3) 
 
+        #Weight for the dogs
+        second_label = tk.Label(self, text = "Weight", font=10)  #Try to have search for size, temperament, and breed
+        second_label.pack(padx = 3, pady = 3) 
+        #Drop down Selections
+        weight_selections = OptionMenu(win, clicked, "Small", "Medium", "Large")
+        
+        #Breed type for the dogs
+        third_label = tk.Label(self, text = "Weight", font=10)  #Try to have search for size, temperament, and breed
+        third_label.pack(padx = 3, pady = 3) 
+        #Drop down Selections
+        breed_selections = OptionMenu(win, clicked, "Small", "Medium", "Large")
+        
+        #Life span of the dogs
+        fourth_label = tk.Label(self, text = "Weight", font=10)  #Try to have search for size, temperament, and breed
+        fourth_label.pack(padx = 3, pady = 3) 
+        #Drop down Selections
+        lifespan_selections = OptionMenu(win, clicked, "Small", "Medium", "Large")
+        
         #A button to go to the search page
         first_button = tk.Button(self, text ="Show Results", command = self.results) 
         first_button.pack(padx= 5, pady = 5)
